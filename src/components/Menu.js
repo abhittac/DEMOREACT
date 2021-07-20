2728efc504286fb866cc93ed5dd8f2af5ab7a42c
abhittac <abhishekchauhan3003@gmail.com>
2021-07-20 23:41:56 +0530

    assignment done

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,70 +1,101 @@
-import React, { Component } from 'react';
-import { Media } from 'reactstrap';
+import React, { Component } from 'react'
 
 class Menu extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
-            selectedDish:null
+
+            selectedDish: null
         }
-        
     }
-    onDishSelected(dish) {
-        this.setState({selectedDish:dish});
-        this.renderDish(dish);
-    }
-    renderDish(dish){
+    renderDish(dish) {
         if(dish!=null){
-            return(
-                <div className="d-flex" >
-                <div className="flex-shrink-0">
-                    <img src={dish.image} className="img-thumbnail" height="80" width="80"  alt={dish.title}/>
+        return (
+            <div className="row">
+                <div className="col-3 col-sm-3 col-md-3">
                 </div>
-                    <div className="flex-grow-1 ms-3">
-                    {dish.title}
-                    </div>
-            
-            </div>
-            )
-        }
-    }
-    render() {
-
-        
-
-        const menu = this.props.dishes.map((dish) => {
-            return (
-                
-                    <div className="d-flex" onClick={()=>{this.onDishSelected(dish);}}>
-                        <div className="flex-shrink-0">
-                            <img src={dish.image} className="img-thumbnail" height="80" width="80"  alt={dish.title}/>
+                <div className="col-3 col-sm-3 col-md-3">
+                    <div className="card" >
+                        <img src={dish.image} className="card-img-top" alt="..." height="180" width="80" />
+                        <div className="card-body">
+                            <h5 className="card-title">{dish.title}</h5>
+                            <p className="card-text">{dish.price}</p>
                         </div>
-                            <div className="flex-grow-1 ms-3">
-                            {dish.title}
-                            </div>
-                    
                     </div>
 
-                    
-                    )
-        })
-                    return (
-                    <>
-                        <div className='container'>
-                            <div className='row'>
-                                <div className='col'>
-                                    {menu}
+                </div>
+                <div className="col-6 col-sm-6 col-md-6">
+                    <div className="accordion" id="accordionExample">
+                        <div className="accordion-item">
+                            <h2 className="accordion-header" id="headingOne">
+                                <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                {dish.avialable[0].name}
+                                </button>
+                            </h2>
+                            <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                <div className="accordion-body">
+                                    <strong>Rating : </strong> {dish.avialable[0].rating}                        </div>
+                            </div>
+                        </div>
+                        <div className="accordion-item">
+                            <h2 className="accordion-header" id="headingTwo">
+                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                {dish.avialable[1].name}
+                                </button>
+                            </h2>
+                            <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                                <div className="accordion-body">
+                                    <strong>Rating :</strong>  {dish.avialable[1].rating}
                                 </div>
                             </div>
-                            <div className='row'>
-                            {this.renderDish(this.state.selectedDish)}
+                        </div>
+                        <div className="accordion-item">
+                            <h2 className="accordion-header" id="headingThree">
+                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                {dish.avialable[2].name}
+                                </button>
+                            </h2>
+                            <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                                <div className="accordion-body">
+                                    <strong>Rating :</strong> {dish.avialable[2].rating}
+                                </div>
                             </div>
                         </div>
-
-                            
-                        
-                    </>
-                    )
+                    </div>
+                </div>
+            </div>
+        )}
     }
-};
-                    export default Menu;
+    render() {
+        return (
+            <div className="container-fluid">
+                <div className="row">
+                    {this.props.dishes.map((dish) => {
+                        return (
+                            <div className="col-12 sm-12 md-3 col-sm-3 mt-4" onClick={() => {
+                                this.setState({ selectedDish: dish })
+                            }}>
+                                <div className="card" >
+                                    <img src={dish.image} className="card-img-top" alt="..." height="180" width="80" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">{dish.title}</h5>
+                                        <p className="card-text">{dish.price}</p>
+                                    </div>
+                                </div>
+
+                            </div>
+
+
+                        )
+                    })}
+
+                </div>
+                <div >{this.renderDish(this.state.selectedDish)}</div>
+            </div>
+        )
+    }
+
+
+}
+export default Menu;
--- src/share/Dish.js
+++ src/share/Dish.js
@@ -3,30 +3,80 @@ export const Dish= [
         "userId": 1,
         "id": 1,
         "title": "Omlet Nachos",
-        "image": '/assets/images/1.jpg'
+        "image": '/assets/images/1.jpg',
+        "price":"$200 /-",
+        "avialable":[
+            { "name":"Hangout in Sec-49",
+             "rating":"✰ ✰ ✰"},
+             { "name":"EF3 in Mathura Road",
+             "rating":"✰ ✰ "},
+             { "name":"Saket in Delhi",
+             "rating":"✰ ✰ ✰ ✰ ✰"}
+
+       ]
     },
     {
         "userId": 2,
         "id": 2,
         "title": "Pizza",
-        "image": '/assets/images/2.jpg'
+        "image": '/assets/images/2.jpg',
+        "price":"$210 /-",
+        "avialable":[
+            { "name":"Hangout in Sec-49",
+             "rating":"✰ ✰ ✰"},
+             { "name":"EF3 in Mathura Road",
+             "rating":"✰ ✰ "},
+             { "name":"Saket in Delhi",
+             "rating":"✰ ✰ ✰ ✰ ✰"}
+
+       ]
     },
     {
         "userId": 3,
         "id": 3,
         "title": "Omlet Spinch",
-        "image": '/assets/images/3.jpg'
+        "image": '/assets/images/3.jpg',
+        "price":"$100 /-",
+        "avialable":[
+            { "name":"Hangout in Sec-49",
+             "rating":"✰ ✰ ✰"},
+             { "name":"EF3 in Mathura Road",
+             "rating":"✰ ✰ "},
+             { "name":"Saket in Delhi",
+             "rating":"✰ ✰ ✰ ✰ ✰"}
+
+       ]
     },
     {
         "userId": 4,
         "id": 4,
         "title": "Noodles",
-        "image": '/assets/images/4.jpg'
+        "image": '/assets/images/4.jpg',
+        "price":"$300 /-",
+        "avialable":[
+            { "name":"Hangout in Sec-49",
+             "rating":"✰ ✰ ✰"},
+             { "name":"EF3 in Mathura Road",
+             "rating":"✰ ✰ "},
+             { "name":"Saket in Delhi",
+             "rating":"✰ ✰ ✰ ✰ ✰"}
+
+       ]
     },
     {
         "userId": 5,
         "id": 5,
         "title": "Fruit Salad",
-        "image": "/assets/images/5.jpg"
+        "image": "/assets/images/5.jpg",
+        "price":"$600 /-",
+        "avialable":[
+            { "name":"Hangout in Sec-49",
+             "rating":"✰ ✰ ✰"},
+             { "name":"EF3 in Mathura Road",
+             "rating":"✰ ✰ "},
+             { "name":"Saket in Delhi",
+             "rating":"✰ ✰ ✰ ✰ ✰"}
+
+       ]
     }
 ];
